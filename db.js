82a146c5209e4cb8198c3d1a21d74408689d45df
MendrikaF <mendrikaandria3@gmail.com>
2025-03-19 09:10:44 +0300

    vita docker

---
+++ db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_NAME || 'ticket_db',
+    port: 3307
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error("Erreur de connexion à MySQL :", err);
+    } else {
+        console.log("✅ Connexion réussie à MySQL !");
+    }
+});
+
+module.exports = db;
